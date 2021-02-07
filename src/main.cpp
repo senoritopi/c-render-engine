2a6096c5aa17117be183775c23911669813aeeb1
luispinonferrer <contacto@magicluiselmago.es>
2021-02-07 03:28:03 +0100

    Algo bresenham + performance

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,56 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor white = TGAColor(255, 255, 255, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &img, TGAColor color) {
-    for(float t = 0.; t < 1.; t += 0.1) {
-        int x = x0 * (1. - t) + x1 * t;
-        int y = y0 * (1. - t) + y1 * t;
-        img.set(x, y, color);
-    }
+    bool transpose = false; 
+	// si la línea es demasiado inclinada la transponemos
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) { 
+        std::swap(x0, y0); 
+        std::swap(x1, y1); 
+        transpose = true; 
+    } 
+	// si la línea es de derecha a izquierda intercambiamos
+    if (x0 > x1) { 
+        std::swap(x0, x1); 
+        std::swap(y0, y1); 
+    } 
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    int error = 2 * dy - dx;
+    int dn = error + dx;
+    int dp = error - dx;
+    int y = y0;
+
+    for (int x = x0; x <= x1; x++) { 
+        if(error < 0){
+            error += dn;
+        } else {
+            error += dp;
+            y += (y1 > y0 ? 1 : -1);
+        }
+
+        if (transpose) { 
+            img.set(y, x, color); 
+        } else { 
+            img.set(x, y, color); 
+        } 
+    } 
 }
 
 int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
+
+    unsigned t0, t1;
+    t0=clock();
+
+    for (int i = 0; i < 30000000; i++) {
+        line(13, 20, 80, 40, image, white);
+    }
+
+    t1 = clock();
+    double time = (double(t1-t0)/CLOCKS_PER_SEC);
+    printf("Execution Time: %f \n", time);
 
     line(10, 10, 10, 90, image, white);
     line(10, 10, 90, 10, image, white);
